c8967d00a8649420d1de053053782e2af46692b1
Idsarth <idsarthdev19@gmail.com>
2020-08-16 13:05:36 -0600

    Errores corregido en el custom hooks useAxios

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,16 +1,15 @@
 import { useCallback, useEffect, useState } from 'react'
 import { useStorage } from './useStorage'
 
+// Import utils
+import { Env } from '../utils'
+
 export const useFetch = ({ url, body, method }, immediate = true) => {
   const { storage } = useStorage('@storage::token', '')
   const [isFetching, setIsFetching] = useState(true)
   const [error, setError] = useState({ hasError: false, message: '' })
-  const [data, setData] = useState()
+  const [data, setData] = useState({})
   const [reFetch, setRefetch] = useState(false)
-
-  useEffect(() => {
-    if (immediate || reFetch) execute()
-  }, [execute, immediate, reFetch])
 
   const execute = useCallback(async () => {
     try {
@@ -18,7 +17,12 @@ export const useFetch = ({ url, body, method }, immediate = true) => {
       headers.append('Content-Type', 'Application/json')
       headers.append('Authorization', `${storage}`)
 
-      const response = await fetch(url, { method, body: JSON.stringify(body), headers })
+      const response = await fetch(`${Env.SERVER_ADDRESS}${url}`,
+        {
+          headers,
+          method,
+          body: JSON.stringify(body)
+        })
       const result = await response.json()
       setData(result)
     } catch (error) {
@@ -27,6 +31,11 @@ export const useFetch = ({ url, body, method }, immediate = true) => {
       setIsFetching(false)
     }
   }, [])
+
+  useEffect(() => {
+    if (immediate || reFetch) execute()
+  }, [execute, immediate, reFetch])
+
 
   return [{ isFetching, error, data }, execute, setRefetch]
 }
--- src/pages/partners.page.jsx
+++ src/pages/partners.page.jsx
@@ -1,30 +1,46 @@
-import React, {useEffect, useRef} from 'react'
+import React, { useEffect, useRef } from 'react'
 import OrgChart from '@balkangraph/orgchart.js'
 
-const Chart = (props) => {
-    const _chart = useRef(null)
-    useEffect(() => {
-        if (_chart.current !== null) {
-            new OrgChart(_chart.current, { nodes: props.nodes, nodeBinding: { field_0: 'name', field_1: 'title' } })
-        }
-    }, [props.nodes])
+// Import hooks
+import { useFetch } from '../hooks/useAxios'
 
-    return (
-        <div id='tree' ref={_chart} />
-    )
+const Chart = (props) => {
+  const _chart = useRef(null)
+  useEffect(() => {
+    if (_chart.current !== null) {
+      new OrgChart(
+        _chart.current,
+        {
+          template: 'olivia',
+          nodes: props.nodes,
+          nodeBinding: {
+            field_0: 'name',
+            field_1: 'title'
+          }
+        })
+    }
+  }, [props.nodes])
+
+  return (
+    <div id='tree' ref={_chart} />
+  )
 }
 
 const PartnersPage = () => {
+  const [{ isFetching, data, error }] = useFetch({ method: 'GET', url: `/matrix/tree/${1}` })
+  useEffect(() => {
+    console.log(isFetching, data, error)
+  }, [isFetching, data, error])
 
-    return (
-        <div>
-            <Chart
-                nodes={[{id: 1, name: "Name1" , title: "Tytle1" },
-                    {id: 2, pid: 1, name: "Name2" , title: "Tytle2" },
-                    {id: 3, pid: 1, name: "Name3" , title: "Tytle3" }]}
-            />
-        </div>
-    )
+  return (
+    <div>
+      <Chart
+        nodes={[{id: 1, name: "Name1" , title: "Tytle1" },
+          {id: 2, pid: 1, name: "Name2" , title: "Tytle2" },
+          {id: 3, pid: 1, name: "Name3" , title: "Tytle3" }]}
+      />
+    </div>
+  )
 }
 
 export default PartnersPage
